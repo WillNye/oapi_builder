84e4ad695f5a65c39c250e65f589755fff3ad556
wbeasley <wbeasley@cylance.com>
2021-07-14 15:49:25 -0500

    Example now includes auth, a post request, a put request and a delete request.

--- example.py
+++ example.py
@@ -2,7 +2,17 @@ from apispec import APISpec
 from marshmallow import fields, Schema
 from apispec.ext.marshmallow import MarshmallowPlugin
 
-from oapi_builder.models import ResponseObject, OperationObject
+from oapi_builder.models import (
+    OAuthFlowObject, OAuthFlowsObject, SecuritySchemeObject, OperationObject, RequestBodyObject, ResponseObject
+)
+
+OAUTH_URL = 'https://service-name.domain.com/api/oauth'
+oauth_security = SecuritySchemeObject(type='oauth2', description='Default OAuth2', flows=OAuthFlowsObject(
+    implicit=OAuthFlowObject(authorization_url=f'{OAUTH_URL}/dialog', scopes={}).to_dict(),
+    authorization_code=OAuthFlowObject(
+        authorization_url=f'{OAUTH_URL}/dialog', token_url=f'{OAUTH_URL}/token', scopes={}
+    ).to_dict()
+).to_dict()).to_dict()
 
 
 spec = APISpec(
@@ -10,12 +20,14 @@ spec = APISpec(
     version="0.1.0",
     openapi_version="3.0.1",
     servers=[
-        {'url': 'http://service-name.s.domain.com', 'description': 'Staging (R02)'},
-        {'url': 'http://service-name.domain.com', 'description': 'Production (US-East-1)'},
-        {'url': 'http://service-name.euc1.domain.com', 'description': 'Production (EU-Central-1)'},
+        {'url': 'https://service-name.s.domain.com', 'description': 'Staging (R02)'},
+        {'url': 'https://service-name.domain.com', 'description': 'Production (US-East-1)'},
+        {'url': 'https://service-name.euc1.domain.com', 'description': 'Production (EU-Central-1)'},
     ],
+    security=[{'default_oauth': []}],
     plugins=[MarshmallowPlugin()],
 )
+spec.components.security_schemes = {'default_oauth': oauth_security}
 
 SUPPORTED_MEDIA_FEEDS = ('reddit', 'instagram', 'hackernews', 'twitter')
 
@@ -25,24 +37,79 @@ class UserFeedGetResponse(Schema):
         description=f'The type of media feed the object represents. <br />'
                     f'Options:<br />   - {"<br />   - ".join(SUPPORTED_MEDIA_FEEDS)}'
     ))
-    username = fields.Str(metadata=dict(
-        description="The user's username for the media feed."
-    ))
+    username = fields.Str(metadata=dict(description="The user's username for the media feed."))
     priority = fields.Int(
         default=1,
         metadata=dict(description='How high this feed should be boosted in the page.', doc_default="1")
     )
 
 
-class UserFeedGetParam(Schema):
+class UserFeedPostRequest(Schema):
+    media_feed = fields.Str(required=True, metadata=dict(
+        description=f'The type of media feed the object represents. <br />'
+                    f'Options:<br />   - {"<br />   - ".join(SUPPORTED_MEDIA_FEEDS)}'
+    ))
+    username = fields.Str(required=True, metadata=dict(description="The user's username for the media feed."))
+    priority = fields.Int(
+        default=1,
+        metadata=dict(description='How high this feed should be boosted in the page.', doc_default="1")
+    )
+
+
+class UserFeedPutRequest(Schema):
+    username = fields.Str(metadata=dict(description="The user's username for the media feed."))
+    priority = fields.Int(
+        metadata=dict(description='How high this feed should be boosted in the page.', doc_default="1")
+    )
+
+
+class UserFeedDetailParam(Schema):
     media_feed = fields.Str(metadata=dict(
         description=f'The type of media feed the object represents. <br />'
                     f'Options:<br />   - {"<br />   - ".join(SUPPORTED_MEDIA_FEEDS)}'
     ))
 
 
-standard_responses = ResponseObject.get_defaults([400, 401, 403, 404, 418, 429])
+standard_get_responses = ResponseObject.get_defaults([400, 401, 403, 404, 418, 429])
+standard_post_responses = ResponseObject.get_defaults([204, 400, 401, 418, 429])
 
+"""
+GET /feed/{media_feed}
+"""
+feed_get_example = UserFeedGetResponse().dump(dict(media_feed='reddit', username='test_user'))
+feed_get_200 = ResponseObject(status_code=200)
+feed_get_200.set_content(UserFeedGetResponse, feed_get_example)
+
+feed_get_operation = OperationObject(description='Retrieve details for a single feed of a user')
+feed_get_operation.add_parameter(UserFeedDetailParam, 'media_feed')
+feed_get_operation.upsert_responses(standard_get_responses + [feed_get_200])
+
+"""
+PUT /feed/{media_feed}
+"""
+feed_put_example = UserFeedPutRequest().dump(dict(priority=5))
+feed_put_request = RequestBodyObject(description='Request body to create a feed for a user', required=True)
+feed_put_request.set_content(UserFeedPostRequest, feed_put_example)
+
+feed_put_response_example = UserFeedGetResponse().dump(dict(media_feed='reddit', username='test_user', priority=5))
+feed_put_200 = ResponseObject(status_code=200)
+feed_put_200.set_content(UserFeedGetResponse, feed_put_response_example)
+
+feed_put_operation = OperationObject(description='Create a feed for a user')
+feed_put_operation.add_parameter(UserFeedDetailParam, 'media_feed')
+feed_put_operation.set_request_body(feed_put_request)
+feed_put_operation.upsert_responses(standard_get_responses + [feed_put_200])
+
+"""
+DELETE /feed/{media_feed}
+"""
+feed_del_operation = OperationObject(description='Remove a user feed')
+feed_del_operation.add_parameter(UserFeedDetailParam, 'media_feed')
+feed_del_operation.upsert_responses(standard_post_responses)
+
+"""
+LIST /feed
+"""
 feed_list_200 = ResponseObject(status_code=200)
 feed_list_200.set_content(UserFeedGetResponse, [
     UserFeedGetResponse().dump(dict(media_feed=media_feed, username=f'test_user.{media_feed}'))
@@ -50,24 +117,26 @@ feed_list_200.set_content(UserFeedGetResponse, [
 ])
 
 feed_list_operation = OperationObject(description='Retrieve a list of feeds for a user')
-feed_list_operation.upsert_responses(standard_responses)
+feed_list_operation.upsert_responses(standard_get_responses)
 feed_list_operation.upsert_responses([feed_list_200])
 
-feed_get_example = UserFeedGetResponse().dump(dict(media_feed='reddit', username='test_user'))
-feed_get_200 = ResponseObject(status_code=200)
-feed_get_200.set_content(UserFeedGetResponse, feed_get_example)
+"""
+POST /feed
+"""
+feed_post_example = UserFeedPostRequest().dump(dict(media_feed='reddit', username='test_user'))
+feed_post_request = RequestBodyObject(description='Request body to create a feed for a user', required=True)
+feed_post_request.set_content(UserFeedPostRequest, feed_post_example)
 
-feed_get_operation = OperationObject(description='Retrieve details for a single feed of a user')
-feed_get_operation.add_parameter(UserFeedGetParam, 'media_feed')
-feed_get_operation.upsert_responses(standard_responses)
-feed_get_operation.upsert_responses([feed_get_200])
+feed_post_operation = OperationObject(description='Create a feed for a user')
+feed_post_operation.set_request_body(feed_post_request)
+feed_post_operation.upsert_responses(standard_post_responses)
 
 spec.path(
     path="/feed",
-    operations=dict(get=feed_list_operation.to_dict())
+    operations=dict(get=feed_list_operation.to_dict(), post=feed_post_operation.to_dict())
 ).path(
     path="/feed/{media_feed}",
-    operations=dict(get=feed_get_operation.to_dict())
+    operations=dict(get=feed_get_operation.to_dict(), delete=feed_del_operation.to_dict())
 )
 
 with open('swagger.yaml', 'w') as f:
